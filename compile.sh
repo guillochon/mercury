8c80ed4ed6821f939689a435cf96037f9ab919b2
James Guillochon <guillochon@gmail.com>
2014-05-06 10:23:14 -0700

    Minor modifications to physical constants to match Mathematica

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 echo "Compiling mercury6 package"
 
-gfortran element6.for -o element6
-gfortran close6.for -o close6
-gfortran mercury6_2.for -o mercury6
+ifort -O3 element6.for -o element6
+ifort -O3 close6.for -o close6
+ifort -O3 mercury6_2.for -o mercury6
 
 echo "Create sample data files? (yes/no)"
 read create_samples
